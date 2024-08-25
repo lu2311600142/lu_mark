c5bdd6d46450cc71b397e88b1b9a3627d7e05120
lu2311600142 <lumark868@gmail.com>
2024-08-25 00:19:46 -0700

    update
    add

--- mainlu.cpp
+++ mainlu.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <string>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <direct.h>
+#include <windows.h>
+#include <io.h>
 
+using namespace std;
